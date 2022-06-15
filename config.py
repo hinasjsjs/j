c72a1c54d9014b355e98c2e4acb799dc9772d621
hinasjsjs <107490837+hinasjsjs@users.noreply.github.com>
2022-06-15 18:46:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5596614242:AAHN_Ob2lnFR_dh9jE8Z1uNIHGFv9UVNtx8")
-SUDO = int(os.environ.get("SUDO", "1983379011"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "1913685619:AAE-y6Vim44l2fYTvyxMe0biI1f_yU8iuKA")
+SUDO = int(os.environ.get("SUDO", "1571471419"))
 HEROKU = os.environ.get("HEROKU", "darken15")
 APP_URL = "https://"+ HEROKU +".herokuapp.com/" + BOT_TOKEN
